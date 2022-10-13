7ece4ad63bc65993eb5060a51fad0d2e6b43eeda
Manuu1409 <manuracingclub61@gmail.com>
2022-10-13 11:30:41 -0300

    error en el router arreglado

--- router.php
+++ router.php
@@ -5,7 +5,7 @@ require_once './app/controllers/user.controller.php';
 
 define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
 
-$action = 'tabla'; // acción por defecto
+$action = 'tablaAutos'; // acción por defecto
 if (!empty($_GET['action'])) {
     $action = $_GET['action'];
 }
